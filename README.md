1e9269365765ab6dac206fba4e6d2d14f13be70f
Long <sy10511@syss.edu.hk>
2024-11-11 23:54:54 +0800

    init

--- README.md
+++ README.md
@@ -4,12 +4,22 @@ An simple online forum built by me with Python Flask
 # Technology used
 - Flask
 - Jinja
+<<<<<<< HEAD
+=======
+- Prisma
+>>>>>>> 4b8b9a1 (init)
 - SQLite
 
 # Installation
 ```
 pip install -r requirements.txt
 ```
+<<<<<<< HEAD
+=======
+```
+prisma generate
+```
+>>>>>>> 4b8b9a1 (init)
 
 # Usage
 ```
--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask, request, redirect, url_for, render_template
+<<<<<<< HEAD
 import sqlite3
 
 
 app = Flask(__name__)
+=======
+from prisma import Prisma
+
+
+app = Flask(__name__)
+prisma = Prisma()
+>>>>>>> 4b8b9a1 (init)
 
 
 @app.route("/")
@@ -10,6 +18,7 @@ def index():
     return render_template("index.html")
 
 
+<<<<<<< HEAD
 @app.route("/new", methods=["GET", "POST"])
 def new_post():
     if request.method == "POST":
@@ -19,11 +28,33 @@ def new_post():
             c = conn.cursor()
             c.execute("INSERT INTO posts (title, content) VALUES (?, ?)", (title, content))
             conn.commit()
+=======
+async def create_post(title, content):
+    await prisma.connect()
+
+    post = await prisma.post.create(
+        data={
+            "title": title,
+            "content": content
+        }
+    )
+
+    await prisma.disconnect()
+
+
+@app.route("/new", methods=["GET", "POST"])
+async def new_post():
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
+        await create_post(title, content)
+>>>>>>> 4b8b9a1 (init)
         return redirect(url_for("home"))
     else:
         return render_template("new_post.html")
 
 
+<<<<<<< HEAD
 @app.route("/home", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
@@ -55,5 +86,48 @@ def home():
     return render_template("home.html", posts=posts)
 
 
+=======
+async def create_answer(post_id, content):
+    await prisma.connect()
+
+    answer = await prisma.answer.create(
+        data={
+            "content": content,
+            "post": {
+                "connect": {"id": post_id}
+            }
+        }
+    )
+
+    await prisma.disconnect()
+
+
+async def get_all_posts():
+    await prisma.connect()
+
+    posts = await prisma.post.find_many(
+        include={"answers": True}  # Including answers from the post
+    )
+
+    await prisma.disconnect()
+
+    return posts
+
+
+@app.route("/home", methods=["GET", "POST"])
+async def home():
+    if request.method == "POST":
+        post_id = int(request.form["post_id"])
+        answer_content = request.form["content"]
+        await create_answer(post_id, answer_content)
+        return redirect(url_for("home"))
+
+    posts = await get_all_posts()
+
+    return render_template("home.html", posts=posts)
+
+
+
+>>>>>>> 4b8b9a1 (init)
 if __name__ == "__main__":
     app.run(debug=True)
